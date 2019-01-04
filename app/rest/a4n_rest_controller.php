f46042483f24a56d177b5914eefa23909b50761d
tomohidemiya <t.miya19890131@gmail.com>
2019-01-04 23:31:17 +0900

    [WIP]RESTハンドラ: 確認メール用のI/F修正

--- app/rest/a4n_rest_controller.php
+++ app/rest/a4n_rest_controller.php
@@ -16,54 +16,20 @@
             // },
             // 2. input validation
             'args' => [
-                'user_id' => [
-                    'required' => true,
-                    'description' => 'ユーザID',
-                    'validation_callback' => function( $var ) {
-                        return ! empty( $var ) && ctype_digit( $var );
-                    },
-                ],
-                'c_number' => [
-                    'required' => true,
-                    'description' => '12桁のカード番号',
-                    'validation_callback' => function( $var ) {
-                        return ! empty( $var ) && ctype_digit( $var ) && strlen( $var ) === 12;
-                    },
-                ],
-                'exp_year' => [
-                    'required' => true,
-                    'description' => '有効期限の年',
-                    'validation_callback' => function( $var ) {
-                        return ! empty( $var ) && ctype_digit( $var );
-                    },
-                ],
-                'exp_month' => [
-                    'required' => true,
-                    'description' => '有効期限の月',
-                    'validation_callback' => function( $var ) {
-                        return ! empty( $var ) && ctype_digit( $var ) && (int)$var >= 1 && (int)$var <= 12;
-                    },
-                ],
-                'cvc' => [
-                    'required' => true,
-                    'description' => 'カード裏面に記載の3桁か4桁のセキュリティチェックコード',
-                    'validation_callback' => function( $var ) {
-                        return ! empty( $var ) && ctype_digit( $var ) && (strlen( $var ) === 3 || strlen( $var ) === 4 );
-                    },
-                ],
-                'name' => [
-                    'required' => true,
-                    'description' => 'カード名義人。ローマ字',
-                    'validation_callback' => function( $var ) {
-                        return ctype_alpha( str_replace( ' ', '', $var ) );
-                    },
-                ],
-                'amount'=> [
+                'price'=> [
                     'required' => true,
                     'description' => '支払い金額',
                     'validation_callback' => function( $var ) {
                         return ! empty( $var ) && ctype_digit( $var );
                     },
+                ],
+                'email' => [
+                    'required' => true,
+                    'description' => 'メールアドレス'
+                ],
+                'deposit_type' => [
+                    'required' => true,
+                    'description' => '決済タイプ'
                 ],
                 'prod_mode'=> [
                     'required' => false,
